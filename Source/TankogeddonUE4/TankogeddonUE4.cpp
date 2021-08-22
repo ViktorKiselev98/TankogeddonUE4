26737ab820418458b88941859456ebd70310ae25
ViktorKiselev98 <77990767+ViktorKiselev98@users.noreply.github.com>
2021-08-22 14:51:28 +0300

    Initial commit

---
+++ Source/TankogeddonUE4/TankogeddonUE4.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TankogeddonUE4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TankogeddonUE4, "TankogeddonUE4" );
---
+++ Source/TankogeddonUE4/TankogeddonUE4GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankogeddonUE4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKOGEDDONUE4_API ATankogeddonUE4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
